0274c7a060d94fdc9d25e6c2a0c6f6b41fd366dd
007rishabhraj <rishabhraj135007@gmail.com>
2024-07-10 23:33:13 +0530

    update header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,66 +1,39 @@
-// import React from "react";
-// import "./Header.css";
-// const Header = () => {
-//   return (
-//     <>
-//       <div className="header">
-//         <img
-//           className="logo-img"
-//           src="../../../utils/logo.png"
-//           alt="img not available"
-//         />
-//         <div className="input">
-//           <input
-//             className="input-box"
-//             type="text"
-//             placeholder="&#xF002; Search for Products..."
-//             />
-//           <button className="search-btn">Search</button>
-//         </div>
-
-//         <div className="options">
-//         <p>Home</p>
-//         <p>Contact Us</p>
-//         <p>Rate us</p>
-//         <p>Profile</p>
-//         </div>
-//       </div>
-//       <hr className="line" />
-//     </>
-//   );
-// };
-
-// export default Header;
-
-import React from "react";
 import { FaUserCircle } from "react-icons/fa";
-import { Outlet } from "react-router-dom";
-// import "tailwindcss/tailwind.css";
+import { Outlet, useLocation } from "react-router-dom";
+import "tailwindcss/tailwind.css";
 
 const Header = () => {
+  const location = useLocation();
+  const hideSearchBar =
+    location.pathname === "/login" || location.pathname === "/signup";
+
   return (
     <div>
-      <header className="bg-white p-4 shadow-md flex items-center font-sans">
-        <img src="logo.png" alt="Logo" className="h-10 mr-4" />
-        <input
-          type="text"
-          placeholder="Search..."
-          className="flex-grow p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 mr-4"
-        />
-        <nav className="flex space-x-4">
-          <a href="/" className="text-gray-700 hover:text-blue-500">
-            Home
-          </a>
-          <a href="/contactus" className="text-gray-700 hover:text-blue-500">
-            Contact Us
-          </a>
-          <a
-            href="/profile"
-            className="text-gray-700 hover:text-blue-500 flex items-center"
-          >
-            <FaUserCircle className="h-6 w-6" />
-          </a>
-        </nav>
+      <header className="bg-white p-4 shadow-md flex items-center justify-between sticky top-0 z-10">
+        <div className="flex items-center space-x-8">
+          <img src="logo.png" alt="Logo" className="h-10" />
+          <div className="space-x-4">
+            <a href="/" className="text-gray-700 hover:text-blue-500">
+              Home
+            </a>
+            <a href="/contactus" className="text-gray-700 hover:text-blue-500">
+              Contact Us
+            </a>
+          </div>
+        </div>
+        {!hideSearchBar && (
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-72 p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        )}
+        <a
+          href="/profile"
+          className="text-gray-700 hover:text-blue-500 flex items-center "
+        >
+          <FaUserCircle className="h-6 w-6" />
+        </a>
       </header>
       <Outlet />
     </div>
--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -58,7 +58,7 @@ const LoginForm = () => {
         <div className="flex items-center justify-between">
           <button
             type="submit"
-            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Login
           </button>
