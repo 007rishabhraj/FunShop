9543e1283846a9eeb71d9a797a6f9f3e2ddc368e
ravikant0917 <21bcs108@nith.ac.in>
2024-07-10 22:29:10 +0530

    add header

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "offwhite": "#F5F5F5",
+      "custom-blue": "#02023D",
+    },
+    fontFamily: {
+      sans: ['Noto Sans', 'sans-serif'],
+    },
   },
   plugins: [],
 };
